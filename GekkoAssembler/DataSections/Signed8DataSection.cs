432f046d4804ddd6a5cde30eb215a1d2bbe4f6f2
Christopher Serr <cryze92@gmail.com>
2015-10-26 00:29:28 +0100

    Fix Bytes not being written properly anymore
    There's no BitConvert.GetBytes overload for byte and sbyte. Instead it
    defaults to short and returns 2 bytes instead.

--- GekkoAssembler/DataSections/Signed8DataSection.cs
+++ GekkoAssembler/DataSections/Signed8DataSection.cs
@@ -5,7 +5,7 @@ namespace GekkoAssembler.DataSections
     public class Signed8DataSection : GekkoDataSection
     {
         public override int Address { get; }
-        public override byte[] Data => BitConverter.GetBytes(Value);
+        public override byte[] Data => new byte[] { (byte)Value };
         public sbyte Value { get; }
 
         public Signed8DataSection(int address, sbyte value)
--- GekkoAssembler/DataSections/Unsigned8DataSection.cs
+++ GekkoAssembler/DataSections/Unsigned8DataSection.cs
@@ -5,7 +5,7 @@ namespace GekkoAssembler.DataSections
     public class Unsigned8DataSection : GekkoDataSection
     {
         public override int Address { get; }
-        public override byte[] Data => BitConverter.GetBytes(Value);
+        public override byte[] Data => new byte[] { Value };
         public byte Value { get; }
 
         public Unsigned8DataSection(int address, byte value)
