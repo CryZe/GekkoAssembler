2b4302ef1d2ca3a025b3428289bf543882ddb924
Christopher Serr <cryze92@gmail.com>
2015-11-01 22:15:05 +0100

    Add a Windows Forms GUI

---
+++ GekkoAssembler.Forms/MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GekkoAssembler.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnAssemble = new System.Windows.Forms.Button();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.bnOpen = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.cmbType, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnAssemble, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bnOpen, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtInput, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtOutput, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(560, 437);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.txtOutput, 2);
+            this.txtOutput.Location = new System.Drawing.Point(367, 32);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(190, 402);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnAssemble
+            // 
+            this.btnAssemble.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnAssemble.Location = new System.Drawing.Point(482, 3);
+            this.btnAssemble.Name = "btnAssemble";
+            this.btnAssemble.Size = new System.Drawing.Size(75, 23);
+            this.btnAssemble.TabIndex = 2;
+            this.btnAssemble.Text = "Assemble";
+            this.btnAssemble.UseVisualStyleBackColor = true;
+            this.btnAssemble.Click += new System.EventHandler(this.btnAssemble_Click);
+            // 
+            // cmbType
+            // 
+            this.cmbType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "Gecko",
+            "Action Replay"});
+            this.cmbType.Location = new System.Drawing.Point(367, 4);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(109, 21);
+            this.cmbType.TabIndex = 3;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(3, 32);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(358, 402);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.WordWrap = false;
+            // 
+            // bnOpen
+            // 
+            this.bnOpen.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.bnOpen.Location = new System.Drawing.Point(3, 3);
+            this.bnOpen.Name = "bnOpen";
+            this.bnOpen.Size = new System.Drawing.Size(75, 23);
+            this.bnOpen.TabIndex = 4;
+            this.bnOpen.Text = "Open";
+            this.bnOpen.UseVisualStyleBackColor = true;
+            this.bnOpen.Click += new System.EventHandler(this.bnOpen_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(420, 320);
+            this.Name = "MainForm";
+            this.Text = "Gekko Assembler";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnAssemble;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Button bnOpen;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ GekkoAssembler.Forms/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GekkoAssembler.Writers;
+
+namespace GekkoAssembler.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            cmbType.SelectedIndex = 0;
+        }
+
+        private void bnOpen_Click(object sender, EventArgs e)
+        {
+            using (var openDialog = new OpenFileDialog() { Filter = "Assembly Files (*.asm)|*.asm|All Files (*.*)|*.*", Multiselect = false })
+            {
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var name = openDialog.FileName;
+                    txtInput.Text = File.ReadAllText(name);
+                }
+            }
+        }
+
+        private void btnAssemble_Click(object sender, EventArgs e)
+        {
+            var assembler = new Assembler();
+            var lines = txtInput.Text.Split('\n');
+            var gekkoAssembly = assembler.AssembleAllLines(lines);
+
+            using (var stream = new MemoryStream())
+            {
+                if (cmbType.SelectedItem.ToString() == "Gecko")
+                {
+                    new GeckoWriter(stream).Visit(gekkoAssembly);
+                }
+                else
+                {
+                    new ActionReplayWriter(stream).Visit(gekkoAssembly);
+                }
+
+                stream.Seek(0, SeekOrigin.Begin);
+                var reader = new StreamReader(stream);
+                var output = reader.ReadToEnd();
+                txtOutput.Text = output;
+            }
+        }
+    }
+}
