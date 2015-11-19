ee0e8c93b93dbd42a05dbbc6adf452dfb9f43a6d
Lioncash <mathew1800@gmail.com>
2015-11-19 09:06:13 -0500

    Assembler: Implement external control instructions

---
+++ GekkoAssembler.Common/GekkoInstructions/ExternalControlInstruction.cs
@@ -0,0 +1,18 @@
+﻿namespace GekkoAssembler.GekkoInstructions
+{
+    public sealed class ExternalControlInstruction : GekkoInstruction
+    {
+        public enum Opcode
+        {
+            ECIWX = 310,
+            ECOWX = 438
+        }
+
+        public override int ByteCode { get; }
+
+        public ExternalControlInstruction(int address, int rD, int rA, int rB, Opcode opcode) : base(address)
+        {
+            ByteCode = (31 << 26 | rD << 21 | rA << 16 | rB << 11 | (int)opcode << 1);
+        }
+    }
+}
