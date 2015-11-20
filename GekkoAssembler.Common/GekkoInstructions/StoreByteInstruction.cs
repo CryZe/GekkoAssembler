9a7087b6f21c00e3c40fd26f26718b2474a26fb0
Lioncash <mathew1800@gmail.com>
2015-11-20 13:20:14 -0500

    Assembler: Implement integer store instructions

--- GekkoAssembler.Common/GekkoInstructions/StoreByteInstruction.cs
+++ GekkoAssembler.Common/GekkoInstructions/StoreByteInstruction.cs
@@ -1,16 +1,16 @@
 ﻿namespace GekkoAssembler.GekkoInstructions
 {
-    public sealed class StoreWordInstruction : GekkoInstruction
+    public sealed class StoreByteInstruction : GekkoInstruction
     {
         public enum Opcode
         {
-            STW  = 36,
-            STWU = 37
+            STB  = 38,
+            STBU = 39
         }
 
         public override int ByteCode { get; }
 
-        public StoreWordInstruction(int address, int rS, int offset, int rA, Opcode opcode) : base(address)
+        public StoreByteInstruction(int address, int rS, int offset, int rA, Opcode opcode) : base(address)
         {
             ByteCode = ((int)opcode << 26 | rS << 21 | rA << 16 | offset & 0xFFFF);
         }
