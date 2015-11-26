49eaf7d72c3ddb1ae1dc1b03b5308315af9e090e
Lioncash <mathew1800@gmail.com>
2015-11-26 08:09:26 -0500

    Assembler: Implement three operand, and multiply FP instructions

---
+++ GekkoAssembler.Common/GekkoInstructions/FloatingPointMultiplyInstruction.cs
@@ -0,0 +1,18 @@
+﻿namespace GekkoAssembler.GekkoInstructions
+{
+    public sealed class FloatingPointMultiplyInstruction : GekkoInstruction
+    {
+        public enum Variant
+        {
+            Single = 59,
+            Double = 63
+        }
+
+        public override int ByteCode { get; }
+
+        public FloatingPointMultiplyInstruction(int address, int frD, int frA, int frC, bool rc, Variant variant) : base(address)
+        {
+            ByteCode = ((int)variant << 26 | frD << 21 | frA << 16 | frC << 6 | 25 << 1 | (rc ? 1 : 0));
+        }
+    }
+}
---
+++ GekkoAssembler.Common/GekkoInstructions/FloatingPointThreeOperandInstruction.cs
@@ -0,0 +1,27 @@
+﻿namespace GekkoAssembler.GekkoInstructions
+{
+    public sealed class FloatingPointThreeOperandInstruction : GekkoInstruction
+    {
+        public enum Variant
+        {
+            Single = 59,
+            Double = 63
+        }
+
+        public enum Opcode
+        {
+            FMADD  = 29,
+            FMSUB  = 28,
+            FNMADD = 31,
+            FNMSUB = 30,
+            FSEL   = 23
+        }
+
+        public override int ByteCode { get; }
+
+        public FloatingPointThreeOperandInstruction(int address, int frD, int frA, int frC, int frB, bool rc, Variant variant, Opcode opcode) : base(address)
+        {
+            ByteCode = ((int)variant << 26 | frD << 21 | frA << 16 | frB << 11 | frC << 6 | (int)opcode << 1 | (rc ? 1 : 0));
+        }
+    }
+}
